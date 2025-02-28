8a9ab5c7ad2f18788f59f64ed6d02b74e9dab69a
Anastasiya Yakovleva <yakovleva.6.0@mail.ru>
2025-02-28 13:59:48 +0300

    even game

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+
 export const helloUser = () => {
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
