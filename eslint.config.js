440bdeb6fc17d12521c2f3cef5f9699bf99b1df2
Anastasiya Yakovleva <yakovleva.6.0@mail.ru>
2025-03-06 23:08:35 +0300

    l

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       'no-console': 'off',
       'import/no-unresolved': 'error',
       'import/order': ['error', { 'newlines-between': 'always' }],
+      'import/prefer-default-export': 'off'
     },
   },
 ]
