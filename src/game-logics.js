7ef8ed86eadcbe7402ce9b85b4248fabb7f09ad4
Anastasiya Yakovleva <yakovleva.6.0@mail.ru>
2025-03-06 19:07:35 +0300

    l

--- src/game-logics.js
+++ src/game-logics.js
@@ -1,25 +1,24 @@
 
-const games = 3
-let win = 0
 const runGame = (gameLogic, name) => {
-  let win = 0;
+  const win = 0;
+  const games = 3;
   while (games > win) {
-    const [question, correctAnswer] = gameLogic()
-    console.log(`Question: ${question}`)
+    const [question, correctAnswer] = gameLogic();
+    console.log(`Question: ${question}`);
     let userAnswer;
     if (process.argv[3]) {
-      userAnswer = process.argv[3]
+      userAnswer = process.argv[3];
     } else {
-      userAnswer = readlineSync.question('Your answer: ')
+      userAnswer = readlineSync.question('Your answer: ');
     }
     if (userAnswer === String(correctAnswer)) {
-      console.log('Correct!')
-      win++;
+      console.log('Correct!');
+      win=+1;
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-      console.log(`Let's try again, ${name}!`)
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
       return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`)
+    };
+  };
+  console.log(`Congratulations, ${name}!`);
 }
