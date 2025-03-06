b323bd86cd6f51b027204559bba6d963e093b0c6
Anastasiya Yakovleva <yakovleva.6.0@mail.ru>
2025-03-06 13:31:57 +0300

    d

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { helloUser } from '../src/cli.js'
-import { progressionStart } from '../src/games/brain-progression.js'
+import { helloUser } from '../src/cli.js';
+import { progressionStart } from '../src/games/brain-progression.js';
 
-const name = helloUser()
-progressionStart(name)
+const name = helloUser();
+progressionStart(name);
