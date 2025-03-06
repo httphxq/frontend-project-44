f85905b75fbc08f19d5bce80b642f36ffa0f31d0
Anastasiya Yakovleva <yakovleva.6.0@mail.ru>
2025-03-06 12:06:15 +0300

    d

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,19 +1,18 @@
-
 import readlineSync from 'readline-sync';
 
 export const primeStart = (name) => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const games = 3;
+  const games = 2;
   let win = 0;
   const factor = 100;
   const complexNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
   while (games > win) {
-    const random = Math.floor(Math.random() * factor);
+    const random = Math.floor(Math.random() * factor); 
     console.log(`Question: ${random}`);
     const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-    const isPrime = complexNumber.includes(random);
-    const correctAnswer = isPrime ? 'yes' : 'no';
+    const isPrime = complexNumber.includes(random); 
+    const correctAnswer = isPrime ? 'yes' : 'no';   
 
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
@@ -21,7 +20,7 @@ export const primeStart = (name) => {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      return; 
+      win += 1
     }
   }
   console.log(`Congratulations, ${name}!`);
